a69281f1768b2025c4f49d12fdc3f433ac420476
chadongmin <cdm2883@naver.com>
2024-04-09 20:42:23 +0900

    dockerfile

--- src/main/java/com/docker/DockerApplication.java
+++ src/main/java/com/docker/DockerApplication.java
@@ -2,9 +2,18 @@ package com.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DockerApplication {
+
+	@RequestMapping("/")
+	public String home()
+	{
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerApplication.class, args);
