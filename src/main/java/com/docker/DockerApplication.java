95ec753a63177f895d34d58ea6652fe3b53af019
chadongmin <cdm2883@naver.com>
2024-04-09 20:43:35 +0900

    Revert "dockerfile"
    This reverts commit a69281f1768b2025c4f49d12fdc3f433ac420476.

--- src/main/java/com/docker/DockerApplication.java
+++ src/main/java/com/docker/DockerApplication.java
@@ -2,18 +2,9 @@ package com.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class DockerApplication {
-
-	@RequestMapping("/")
-	public String home()
-	{
-		return "Hello Docker World";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerApplication.class, args);
